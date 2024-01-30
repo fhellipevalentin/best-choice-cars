85a981825b3bc2d072860927620960a17645c9ef
fhellipevalentin <fhellipereis@outlook.com>
2024-01-30 15:45:13 -0300

    fase 2 - combobox e pesquisa por nome selecionado

--- src/main/java/com/example/demo/controller/CarController.java
+++ src/main/java/com/example/demo/controller/CarController.java
@@ -1,13 +1,42 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.model.Car;
+import com.example.demo.repository.CarRepository;
 
 @Controller
 public class CarController {
-	
-	@GetMapping("/")
-    public String login() {
+
+    private final CarRepository carRepository;
+
+    public CarController(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
         return "index";
+    }
+
+    @GetMapping("/selectBrand")
+    public String selectBrand(Model model) {
+        List<String> distinctBrands = carRepository.findDistinctBrands();
+        model.addAttribute("brands", distinctBrands);
+        model.addAttribute("car", new Car());
+        return "selectBrand";
+    }
+
+    @PostMapping("/getCarByBrand")
+    public String getCarByBrand(@ModelAttribute("car") Car car, Model model) {
+        List<Car> cars = carRepository.findByBrand(car.getBrand());
+        model.addAttribute("cars", cars);
+        return "getCarByBrand";
     }
 }
--- src/main/java/com/example/demo/repository/CarRepository.java
+++ src/main/java/com/example/demo/repository/CarRepository.java
@@ -1,9 +1,16 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.model.Car;
 
 public interface CarRepository extends JpaRepository<Car, Long>{
-
+	
+	List<Car> findByBrand(String brand);
+	
+	@Query("SELECT DISTINCT c.brand FROM Car c")
+    List<String> findDistinctBrands();
 }
